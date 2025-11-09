f99b792e60bb732245ba4aad0733634900fda776
yujin <yujinjeong@gmail.com>
2025-11-09 17:31:21 +0900

    feat: expo 실행

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /**
- * React Native 엔트리포인트
+ * Expo 엔트리포인트
  *
  * 앱 등록 및 초기화
  */
 
-import { AppRegistry } from 'react-native';
+import { registerRootComponent } from 'expo';
 import App from './src/app/App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
